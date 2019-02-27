9d51d2e8083641ac72ee2ddeb61c2b5cbb391c8a
peckpeck20 <peckpeckthebeast@gmail.com>
2019-02-27 21:10:18 +0200

    Delete & update methods - clean up code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import "./App.css";
+
 
 function App() {
   const [todos, setTodos] = useState([{ text: 'milk', isDone: false }, { text: 'bread', isDone: true }]);
-
 
   const addTodo = text => {
     // take a copy of current todos and add a new todo obj
@@ -10,7 +11,6 @@ function App() {
     // update state of todos
     setTodos(newTodos);
   }
-
 
   const markDone = (index) => {
     // takes a copy of current todos 
@@ -21,19 +21,33 @@ function App() {
     setTodos(newTodos)
   }
 
+  const deleteTodo = (index) => {
+    // takes a copy of current todos 
+    const newTodos = [...todos]
+    // deletes todo obj based on its Index
+    newTodos.splice(index, 1)
+    // update state
+    setTodos(newTodos)
+  }
+
   return (
-    <div>
-      <TodoForm addTodo={addTodo} />
-      <ul>
+    <div className="app">
+      <div className="todo-list">
         {todos.map((item, index) => (
-          item.isDone ? <li style={{ textDecoration: "line-through" }} key={index}>{item.text} <button onClick={() => markDone(index)}>undo</button></li> :
-            <li key={index}>{item.text} <button onClick={() => markDone(index)}>done</button></li>
+          <TodoItem
+            todo={item}
+            key={index}
+            index={index}
+            markDone={markDone}
+            deleteTodo={deleteTodo}
+          />
         ))}
-      </ul>
+        <TodoForm addTodo={addTodo} />
+      </div>
     </div>
   );
 }
-// props can be also be destructured like this => todoform({propName})
+
 function TodoForm(props) {
   const [newTodo, setNewTodo] = useState("");
 
@@ -46,14 +60,31 @@ function TodoForm(props) {
     }
     // adds new todo with the fn passed with props
     props.addTodo(newTodo);
+    // clear state
     setNewTodo('')
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" value={newTodo} onChange={e => setNewTodo(e.target.value)} />
+      <input className="input" type="text" placeholder="add new todo" value={newTodo} onChange={e => setNewTodo(e.target.value)} />
     </form>
   )
 }
+// props can be also be destructured like this => TodoItem({propName}) & fn components can be made as () => fns
+const TodoItem = ({ todo, index, markDone, deleteTodo }) => {
+  return (
+    <div
+      className="todo"
+      style={{ textDecoration: todo.isDone ? "line-through" : '' }}
+    >
+      {todo.text}
+      <div>
+        <button onClick={() => markDone(index)}>done</button>
+        <button onClick={() => deleteTodo(index)}>delete</button>
+      </div>
+    </div>
+  )
+}
+
 
 export default App;
